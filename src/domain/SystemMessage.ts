b0938ea78d17b77071a94ed160e4c4818fc53b3a
AdeildoNeto <n.santos1897@gmail.com>
2021-01-18 20:42:02 -0300

    [feat]: constants file to use in system standard messages

---
+++ src/domain/SystemMessage.ts
@@ -0,0 +1,9 @@
+export class SystemMessage {
+
+    static USER_NOT_FOUND = "User not found!";
+
+    static USERNAME_INVALID = "Username invalid or already taken! Please chooser another username!";
+
+    static WELCOME = "Welcome to our Chat! Please choose a username!";
+
+}
